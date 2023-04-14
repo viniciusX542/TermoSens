794b22f3b67ed479e1a42b8551712be6654962eb
Mauro Teixeira <mevyness@gmail.com>
2023-04-13 21:10:41 -0300

    Mauro | 13/04 | 21:10

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./src/css/index.css">
     <link rel="icon" href="./logo/icons8-termômetro-64.png">
     <title>Sensor de Temperatura</title>
 </head>
