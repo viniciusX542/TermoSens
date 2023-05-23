b7917fbf334f7dd99b4295d4a12d21405351b2a4
Danielfilipe123 <dfsa1@discente.ifpe.edu.br>
2023-05-23 12:02:46 -0300

    teste

--- src/php/session/login.php
+++ src/php/session/login.php
@@ -6,14 +6,14 @@ if($_SERVER['REQUEST_METHOD'] != 'POST'){
     $fp = fopen('../crud-user/usuarios.csv','r');
 
     while(($row = fgetcsv($fp)) !== false){
-        if($row[3] ==$_POST['email'] && $row[4] == $_POST['senha']){
+        if($row[1] ==$_POST['email'] && $row[2] == $_POST['senha']){
             if(isset($_POST['auth'])) {
             echo "certo";
             exit();
             }
             session_start();
             $_SESSION['logar'] = $row[0];
-            $_SESSION['autht'] == true;
+            // $_SESSION['autht'] == true;
             header('location: ../crud-sensor/indexSen.php ');
             exit();
         }
