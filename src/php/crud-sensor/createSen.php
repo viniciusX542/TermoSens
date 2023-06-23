c4c8f8be29278154e8890fac311a484305bb666f
viniciusX542 <mendonvinicius@gmail.com>
2023-06-23 10:39:34 -0300

    correção equivocada
    abs

--- src/php/crud-sensor/createSen.php
+++ src/php/crud-sensor/createSen.php
@@ -2,9 +2,14 @@
 
 $identificador = $_POST['identificador'];
 $nome = $_POST['nome'];
+$ip = $_POST['ip'];
+$diasmedidos = $_POST['diasmedidos'];
+$hora = $_POST['hora'];
+$temperatura = $_POST['temperatura'];
+$intervalo = $_POST['intervalo'];
 
 $fp = fopen('sensores.csv', 'a');
-fputcsv($fp, [$identificador, $nome]);
+fputcsv($fp, [$identificador, $nome,$ip,$diasmedidos,$hora,$temperatura,$intervalo]);
 
 header('location: listaSen.php');
 
