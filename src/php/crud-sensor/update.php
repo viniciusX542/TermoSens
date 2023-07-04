15971b4569d51e1a56c8aee2a128dfa983d9b325
viniciusX542 <mendonvinicius@gmail.com>
2023-07-04 14:12:15 -0300

    a

--- src/php/crud-sensor/update.php
+++ src/php/crud-sensor/update.php
@@ -1,6 +1,11 @@
 <?php
     $identificador = $_POST['identificador'];
     $nome = $_POST['nome'];
+    $ip = $_POST['ip'];
+    $diasmedidos = $_POST['diasmedidos'];
+    $hora = $_POST['hora'];
+    $temperatura = $_POST['temperatura'];
+    $intervalo = $_POST['intervalo'];
    
     $temName = tempnam('.', '');
 
